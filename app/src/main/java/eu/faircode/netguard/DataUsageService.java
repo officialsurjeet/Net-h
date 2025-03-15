ee55a3a035df0dcc3f0d7994294e796c17cf4ebc
officialsurjeet <44050893+officialsurjeet@users.noreply.github.com>
2025-03-15 18:55:19 +0530

    Update DataUsageService.java

--- app/src/main/java/eu/faircode/netguard/DataUsageService.java
+++ app/src/main/java/eu/faircode/netguard/DataUsageService.java
@@ -82,7 +82,7 @@ public class DataUsageService extends Service
 				.setContentIntent(pendingIntent)
 				.setOngoing(true) // Make the notification persistent
 				.setPriority(NotificationCompat.PRIORITY_MAX)
-			.setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+			//.setStyle(new NotificationCompat.DecoratedCustomViewStyle())
             .setCustomContentView(customView)
             .setCustomBigContentView(customView) // Set custom big content view to expand by default
           
