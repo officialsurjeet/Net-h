ae81b16726e5e18ecf911ca8ae3ee53433f014a1
officialsurjeet <44050893+officialsurjeet@users.noreply.github.com>
2025-03-15 23:34:20 +0530

    Update DataUsageService.java

--- app/src/main/java/eu/faircode/netguard/DataUsageService.java
+++ app/src/main/java/eu/faircode/netguard/DataUsageService.java
@@ -122,6 +122,24 @@ public class DataUsageService extends Service
 		}
 	};
 
+
+	private String formatBytes(long bytes) 
+	{
+		if (bytes < 1024) 
+		{
+			return bytes + " B";
+		} else if (bytes < 1024 * 1024) 
+		{
+			return String.format("%.2f KB", bytes / 1024.0);
+		} else if (bytes < 1024 * 1024 * 1024) 
+		{
+			return String.format("%.2f MB", bytes / (1024.0 * 1024.0));
+		} else 
+		{
+			return String.format("%.2f GB", bytes / (1024.0 * 1024.0 * 1024.0));
+		}
+	}
+	
     private String formatSpeedIcon(double speed) {
 		if (speed < 1024) {
 			return String.format("%.0f\nB/s", speed);
